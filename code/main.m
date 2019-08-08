5e82772a77628918361eecd1a1972803b40b9e4b
Meet001 <meetkathiriya12345@gmail.com>
2019-08-08 13:34:51 +0530

    First Commit

---
+++ code/main.m
@@ -0,0 +1,52 @@
+
+
+%%denoising
+image = im2double(imread('child.png'));
+img = denoising(image,3,1,5);
+
+filter = fspecial('gaussian',5,3);
+guass = imfilter(image,filter);
+
+figure ; subplot(1,3,1) ; imshow(image) ;subplot(1,3,2) ; imshow(guass,[]); subplot(1,3,3) ; imshow(img,[]);
+
+%%inpainting
+image = im2double(imread('specsGuy.jpg'));
+mask = im2double(imread('specsmask.png'));
+
+inpainting(image,mask,5,10,10);
+
+
+%%restoration
+image = im2double(imread('obama.bmp'));
+image = image(1:2:end,1:2:end,:);
+[row,col,channels] = size(image);
+out = zeros(size(image));
+mask = ones(row,col);
+bilinearLenna = zeros(size(image));
+
+%creating a mask and removinf pixels
+for i = 1:row
+    for j = 1:col
+        if(mod(i,2) == 1 && mod(j,2) == 0)
+            continue;
+        end
+        
+        if(mod(i,2) == 0 && mod(j,2) == 1)
+            continue;
+        end
+        mask(i,j) = 0;
+        out(i,j,:) = image(i,j,:);
+    end
+end
+
+img = inpainting(out,mask,3,1,10);
+
+figure ; subplot(1,3,1) ; imshow(image) ; subplot(1,3,2) ; imshow(out); subplot(1,3,3) ; imshow(img);
+
+
+
+
+
+
+
+
